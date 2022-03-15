6034eaf2e7129d6c3c64cd5ce8290aeb8708414c
Jerry Jiang <37967938+jjiang0595@users.noreply.github.com>
2022-03-14 23:48:23 -0400

    Update fileio.py

--- Learn Python Programming Masterclass - Udemy/Section 8 - Input and Output/fileio.py
+++ Learn Python Programming Masterclass - Udemy/Section 8 - Input and Output/fileio.py
@@ -9,8 +9,30 @@
 #     for line in jabber:
 #         if "JAB" in line.upper():
 #             print(line, end='')
+
+# with open("sample.txt", 'r') as jabber:
+#     line = jabber.readline()
+#     while line:
+#         print(line, end='')
+#         line = jabber.readline()
+
+# with open("sample.txt", 'r') as jabber:
+#     lines = jabber.readlines()
+# print(lines)
+#
+# for line in lines:
+#     print(line, end="")
+
 with open("sample.txt", 'r') as jabber:
-    line = jabber.readline()
-    while line:
-        print(line, end='')
-        line = jabber.readline()
+    lines = jabber.readlines()
+print(lines)
+
+for line in lines[::-1]:
+    print(line, end="")
+
+with open("sample.txt", 'r') as jabber:
+    lines = jabber.read()
+print(lines)
+
+for line in lines[::-1]:
+    print(line, end="")
